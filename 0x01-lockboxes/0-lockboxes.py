7bd52e5beff9bf15d2cf2ff950059028abe4e854
Joseph Endale <josi.endal45@gmail.com>
2023-07-03 17:52:11 +0300

    Create 0-lockboxes.py

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,21 @@
+def canUnlockAll(boxes):
+    """
+    Determines if all the boxes can be opened.
+
+    Parameters:
+    boxes (list): A list of lists of integers 
+    representing the boxes and their keys.
+
+    Returns:
+    bool: True if all boxes can be opened, False otherwise.
+    """
+    opened_boxes = set()
+    opened_boxes.add(0)
+    keys = boxes[0]
+    # Iterate over the keys until no new keys are found
+    while keys:
+        key = keys.pop(0)
+        if key < len(boxes) and key not in opened_boxes:
+            opened_boxes.add(key)
+            keys.extend(boxes[key])
+    return len(opened_boxes) == len(boxes)
