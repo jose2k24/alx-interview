523c00ba6ea95a86020ab06dc306f4a0f471dc6b
Joseph Endale <josi.endal45@gmail.com>
2023-08-09 16:29:19 +0300

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,23 +1,5 @@
 #!/usr/bin/python3
 
-python
-"""
-This module reads from standard input line by line and computes metrics.
-
-Input format: <IP Address> - [<date>] "GET /projects/260 HTTP/1.1" <status code> <file size>
-After every 10 lines and/or a keyboard interruption (CTRL + C), print these statistics from the beginning:
-- Total file size: File size: <total size>, where <total size> is the sum of all previous <file size>.
-- Number of lines by status code:
-  - Possible status codes: 200, 301, 400, 401, 403, 404, 405, and 500.
-  - If a status code doesn’t appear or is not an integer, don’t print anything for this status code.
-  - Format: <status code>: <number>
-  - Status codes should be printed in ascending order.
-
-Example usage:
-$ cat input.txt | python metrics.py
-
-"""
-
 import sys
 
 def print_msg(dict_sc, total_file_size):
