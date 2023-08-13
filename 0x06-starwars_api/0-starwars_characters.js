dde6cd2e482dc11d908176b4c8d2332dfb63431a
Joseph Endale <josi.endal45@gmail.com>
2023-08-13 19:11:35 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,53 +1,44 @@
 #!/usr/bin/node
 
 const request = require('request');
+const args = process.argv.slice(2);
 
-const movieId = process.argv[2];
-const filmEndPoint = 'https://swapi-api.hbtn.io/api/films/' + movieId;
-let people = [];
-const names = [];
+if (args.length !== 1) {
+  console.error('Usage: 0-starwars_characters.js <movie_id>');
+  process.exit(1);
+}
 
-const requestCharacters = async () => {
-  await new Promise(resolve => request(filmEndPoint, (err, res, body) => {
-    if (err || res.statusCode !== 200) {
-      console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-    } else {
-      const jsonBody = JSON.parse(body);
-      people = jsonBody.characters;
-      resolve();
-    }
-  }));
-};
+const movieId = args[0];
+const url = `https://swapi.dev/api/films/${movieId}/`;
 
-const requestNames = async () => {
-  if (people.length > 0) {
-    for (const p of people) {
-      await new Promise(resolve => request(p, (err, res, body) => {
-        if (err || res.statusCode !== 200) {
-          console.error('Error: ', err, '| StatusCode: ', res.statusCode);
-        } else {
-          const jsonBody = JSON.parse(body);
-          names.push(jsonBody.name);
-          resolve();
-        }
-      }));
-    }
-  } else {
-    console.error('Error: Got no Characters for some reason');
+request(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
   }
-};
 
-const getCharNames = async () => {
-  await requestCharacters();
-  await requestNames();
-
-  for (const n of names) {
-    if (n === names[names.length - 1]) {
-      process.stdout.write(n);
-    } else {
-      process.stdout.write(n + '\n');
-    }
+  if (response.statusCode !== 200) {
+    console.error(`Unexpected status code: ${response.statusCode}`);
+    process.exit(1);
   }
-};
 
-getCharNames();
+  const movie = JSON.parse(body);
+  const characters = movie.characters;
+
+  characters.forEach((characterUrl) => {
+    request(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        process.exit(1);
+      }
+
+      if (response.statusCode !== 200) {
+        console.error(`Unexpected status code: ${response.statusCode}`);
+        process.exit(1);
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+    });
+  });
+});
