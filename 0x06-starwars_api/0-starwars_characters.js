ce08bbb23b703d2d0a0c321df84a75390adcfefe
Joseph Endale <josi.endal45@gmail.com>
2023-08-13 19:28:46 +0300

    Update 0-starwars_characters.js

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,25 +1,51 @@
 #!/usr/bin/node
+
 const request = require('request');
-const API_URL = 'https://swapi-api.hbtn.io/api';
 
-if (process.argv.length > 2) {
-  request(`${API_URL}/films/${process.argv[2]}/`, (err, _, body) => {
-    if (err) {
-      console.log(err);
-    }
-    const charactersURL = JSON.parse(body).characters;
-    const charactersName = charactersURL.map(
-      url => new Promise((resolve, reject) => {
-        request(url, (promiseErr, __, charactersReqBody) => {
-          if (promiseErr) {
-            reject(promiseErr);
-          }
-          resolve(JSON.parse(charactersReqBody).name);
-        });
-      }));
-
-    Promise.all(charactersName)
-      .then(names => console.log(names.join('\n')))
-      .catch(allErr => console.log(allErr));
-  });
+if (process.argv.length !== 3) {
+  console.error('Usage: 0-starwars_characters.js <movie_id>');
+  process.exit(1);
 }
+
+const movieId = process.argv[2];
+const url = `https://swapi.dev/api/films/${movieId}/`;
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    process.exit(1);
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Unexpected status code: ${response.statusCode}`);
+    process.exit(1);
+  }
+
+  const movie = JSON.parse(body);
+  const charactersUrls = movie.characters;
+
+  const printCharacters = (charactersUrls) => {
+    if (charactersUrls.length === 0) {
+      return;
+    }
+
+    const characterUrl = charactersUrls.shift();
+    request.get(characterUrl, (error, response, body) => {
+      if (error) {
+        console.error(error);
+        process.exit(1);
+      }
+
+      if (response.statusCode !== 200) {
+        console.error(`Unexpected status code: ${response.statusCode}`);
+        process.exit(1);
+      }
+
+      const character = JSON.parse(body);
+      console.log(character.name);
+      printCharacters(charactersUrls);
+    });
+  };
+
+  printCharacters(charactersUrls);
+});
